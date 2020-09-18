9ee22138cb0d6777c7826780c0496e3337a4daec
Amy J. Ko <ajko@uw.edu>
2020-09-17 18:46:44 -0700

    Added more words to exclude.

--- peruse.js
+++ peruse.js
@@ -81,7 +81,7 @@ class Peruse extends React.Component {
 	computeIndex(text) {
 
 		// Build a list of common words
-		var commonWords = _.keyBy(["a","about","all","also","and","as","at","be","because","but","by","can","come","could","day","do","even","find","first","for","from","get","give","go","have","he","her","here","him","his","how","I","if","in","into","it","its","just","know","like","look","make","man","many","me","more","my","new","no","not","now","of","on","one","only","or","other","our","out","people","say","see","she","so","some","take","tell","than","that","the","their","them","then","there","these","they","thing","think","this","those","time","to","two","up","use","very","want","was","way","we","well","what","when","which","who","will","with","would","year","yes","you","your"]);
+		var commonWords = _.keyBy(["a","about","all","also","and","as","at","be","because","but","by","can","come","could","day","do","does","even","find","first","for","from","get","give","go","have","he","her","here","him","his","how","I","if","in","into","it","its","just","know","like","look","make","man","many","me","more","my","new","no","not","now","of","on","one","only","or","other","our","out","people","say","see","she","so","some","take","tell","than","that","the","their","them","then","there","these","they","thing","think","this","those","time","to","two","up","use","very","want","was","way","we","well","went","what","when","which","who","will","with","would","year","yes","you","your"]);
 
 		// Get all the text in the chapter.
         var text = Parser.parseChapter(text).toText();
