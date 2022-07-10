0be2128dd3f6f5d296e2642c14a82b86a07b9a2d
Amy J. Ko <ajko@uw.edu>
2022-07-10 10:26:26 -0700

    A bit of style cleanup.

--- src/views/app/Home.tsx
+++ src/views/app/Home.tsx
@@ -5,6 +5,7 @@ export default function Home() {
 
     return <>
         <h1>Welcome to <strong>Bookish</strong></h1>
+
         <p>Where books are bound for the web.</p>
 
         <p><Link to="read">Read a book</Link>.</p>
