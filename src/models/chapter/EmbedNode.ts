5d70558ba5ba58a722f27de3e8806ccca9e12d00
Amy J. Ko <ajko@uw.edu>
2022-10-01 12:13:20 -0700

    Fixed URL separator defect.

--- src/models/chapter/EmbedNode.ts
+++ src/models/chapter/EmbedNode.ts
@@ -5,13 +5,16 @@ import { TextNode } from "./TextNode";
 import { BlockNode } from "./BlockNode";
 import { CaretRange } from "./Caret";
 
+// This is what we use to encode URLs in the URL field of the embed node.
+const URL_SEPARATOR = "*";
+
 export class EmbedNode extends BlockNode {
 
     /**
-     * The URL is actually a space separated set of up to two URLs. The first is a full-size image URL, the second is a smaller image.
+     * The URL is actually a list of up to two URLs. The first is a full-size image URL, the second is a smaller image.
      * These are used for responsive rendering and to limit bandwidth usage. Any image URLs after the second are ignored.
      */
-    readonly #url: string;
+    readonly #urls: string;
     readonly #description: string;
     readonly #caption: FormatNode;
     readonly #credit: FormatNode;
@@ -21,7 +24,7 @@ export class EmbedNode extends BlockNode {
 
         super();
 
-        this.#url = url;
+        this.#urls = url;
         this.#description = description;
         this.#caption = caption === undefined ? new FormatNode("", [ new TextNode() ]) : caption.withTextIfEmpty();
         this.#credit = credit == undefined ? new FormatNode("", [ new TextNode() ]) : credit.withTextIfEmpty();
@@ -30,11 +33,11 @@ export class EmbedNode extends BlockNode {
     }
 
     getType() { return "embed"; }
-    getURL() { return this.#url.split(" ")[0]; }
-    getSmallURL() { return this.isLocal() ? "images/small/" + this.getURL() : this.hasSmallURL() ? this.#url.split(" ")[1] : this.getURL(); }
-    hasSmallURL() { return this.#url.split(" ").length > 1 && this.#url.split(" ")[1].trim().length > 0; }
-    isHosted() { return this.#url.includes("bookish"); }
-    isLocal() { return !this.#url.startsWith("http"); }
+    getURL() { return this.#urls.split(URL_SEPARATOR)[0]; }
+    getSmallURL() { return this.isLocal() ? "images/small/" + this.getURL() : this.hasSmallURL() ? this.#urls.split(URL_SEPARATOR)[1] : this.getURL(); }
+    hasSmallURL() { return this.#urls.split(URL_SEPARATOR).length > 1 && this.#urls.split(URL_SEPARATOR)[1].trim().length > 0; }
+    isHosted() { return this.#urls.includes("bookish"); }
+    isLocal() { return !this.#urls.startsWith("http"); }
     getDescription() { return this.#description; }
     getCaption() { return this.#caption; }
     getCredit() { return this.#credit; }
@@ -51,11 +54,11 @@ export class EmbedNode extends BlockNode {
     
     toText(): string { return this.#caption.toText(); }
     toBookdown(): string {
-        return `|${this.#url}|${this.#description}|${this.#caption.toBookdown()}|${this.#credit.toBookdown()}|`;
+        return `|${this.#urls}|${this.#description}|${this.#caption.toBookdown()}|${this.#credit.toBookdown()}|`;
     }
     toJSON() {
         return {
-            url: this.#url,
+            url: this.#urls,
             alt: this.#description,
             caption: this.#caption.toText(),
             credit: this.#credit.toText()
@@ -63,7 +66,7 @@ export class EmbedNode extends BlockNode {
     }
 
     copy() {
-        return new EmbedNode(this.#url, this.#description, this.#caption.copy(), this.#credit.copy(), this.#position) as this;
+        return new EmbedNode(this.#urls, this.#description, this.#caption.copy(), this.#credit.copy(), this.#position) as this;
     }
 
     withChildReplaced(node: Node, replacement: Node | undefined) {
@@ -71,7 +74,7 @@ export class EmbedNode extends BlockNode {
         const newCredit = (node === this.#credit && (replacement === undefined || replacement instanceof FormatNode)) ? replacement : this.#credit;
         return newCaption || newCredit ?
             new EmbedNode(
-                this.#url,
+                this.#urls,
                 this.#description,
                 newCaption, 
                 newCredit, 
@@ -89,11 +92,11 @@ export class EmbedNode extends BlockNode {
         return new EmbedNode(url, this.#description, this.#caption, this.#credit, this.#position); 
     
     }
-    withURLs(url: string, thumbnail: string) { return new EmbedNode(url + " " + thumbnail, this.#description, this.#caption, this.#credit, this.#position); }
-    withDescription(description: string) { return new EmbedNode(this.#url, description, this.#caption, this.#credit, this.#position); }
-    withCaption(caption: FormatNode) { return new EmbedNode(this.#url, this.#description, caption, this.#credit, this.#position); }
-    withCredit(credit: FormatNode) { return new EmbedNode(this.#url, this.#description, this.#caption, credit, this.#position); }
-    withPosition(position: Position) { return new EmbedNode(this.#url, this.#description, this.#caption, this.#credit, position); }
+    withURLs(url: string, thumbnail: string) { return new EmbedNode(url + URL_SEPARATOR + thumbnail, this.#description, this.#caption, this.#credit, this.#position); }
+    withDescription(description: string) { return new EmbedNode(this.#urls, description, this.#caption, this.#credit, this.#position); }
+    withCaption(caption: FormatNode) { return new EmbedNode(this.#urls, this.#description, caption, this.#credit, this.#position); }
+    withCredit(credit: FormatNode) { return new EmbedNode(this.#urls, this.#description, this.#caption, credit, this.#position); }
+    withPosition(position: Position) { return new EmbedNode(this.#urls, this.#description, this.#caption, this.#credit, position); }
     
     withContentInRange(range: CaretRange): this | undefined {
 
@@ -102,7 +105,7 @@ export class EmbedNode extends BlockNode {
         const newCaption = this.#credit.contains(range.end.node) ? new FormatNode("", [ new TextNode() ] ) : this.#caption.withContentInRange(range);
         if(newCaption === undefined || newCredit === undefined) return;
 
-        return new EmbedNode(this.#url, this.#description, newCaption, newCredit, this.#position) as this;
+        return new EmbedNode(this.#urls, this.#description, newCaption, newCredit, this.#position) as this;
         
     }
 
