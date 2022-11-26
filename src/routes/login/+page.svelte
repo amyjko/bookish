b5272e2f29e70d6e2e3bc0108a6e3c68feec817d
Amy J. Ko <ajko@uw.edu>
2022-11-26 09:56:47 -0800

    Disable login until valid email.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,8 +3,8 @@
 	import Alert from "$lib/components/page/Alert.svelte";
     import Title from "$lib/components/page/Title.svelte";
 
-	let email: HTMLInputElement;
-	let auth = getAuth()
+	let input: HTMLInputElement;
+	let user = getAuth()
 
 	// Not loading by default
 	let loading = false;
@@ -12,16 +12,19 @@
 	// Track feedback with state
 	let feedback = "";
 
+	// Recompute the email based on the latest input value.
+	let email = "";
+
 	async function handleSubmit() {
 
 		// Enter loading state, try to login and wait for it to complete, and then leave loading state.
 		try {
 			// Give some feedback when loading.
 			loading = true;
-			await $auth.login(email.value);
+			await $user.login(email);
 			feedback = "Check your email for a login link.";
 		} catch(err) {
-			feedback = "Uh oh! Couldn't connect to the server.";
+			feedback = "Couldn't connect to the server.";
 			console.error(err);
 		} finally {
 			loading = false;
@@ -35,8 +38,8 @@
 
 <p>We'll send you an email each time to login, no password required.</p>
 
-<form on:submit|preventDefault={handleSubmit}>
-    <input autocomplete="username" type="email" placeholder="email" bind:this={email} required disabled={loading} /> <button type="submit" disabled={loading}>Login</button>
+<form on:submit|preventDefault={handleSubmit} disabled={loading}>
+    <input autocomplete="username" type="email" placeholder="email" bind:this={input} required disabled={loading} on:input={() => email = input.value }/> <button type="submit" disabled={loading || !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)}>Login</button>
 </form>
 
 {#if feedback }
