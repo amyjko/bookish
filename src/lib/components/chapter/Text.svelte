4c33b32d8751396714432610b17356dfd61ffed0
Amy J. Ko <ajko@uw.edu>
2022-12-17 12:21:59 -0800

    Don't highlight text being edited.

--- src/lib/components/chapter/Text.svelte
+++ src/lib/components/chapter/Text.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type TextNode from "$lib/models/chapter/TextNode";
-    import { getChapter } from "../page/Contexts";
+    import { getCaret, getChapter, isEditable } from "../page/Contexts";
 
     export let node: TextNode;
     export let placeholder: string | undefined = undefined; 
@@ -8,6 +8,8 @@
     $: text = node.getText();
 
     let context = getChapter();
+    let editable = isEditable();
+    let caret = getCaret();
 
     function replaceMultipleSpacesWithNonBreakingSpaces(original: string) {
         let revisedText = ""
@@ -24,43 +26,49 @@
     // Compute highlights, if highlighted
     let segments: [string, boolean][] | undefined = undefined;
     $: {   
-        // Replace any spaces at the beginning or end of the string with explicit non-breaking spaces to ensure that they render.
-        text = replaceMultipleSpacesWithNonBreakingSpaces(text);
 
-        // If the text ends with a newline, render a non-breaking space at the end.
-        if(text.length > 0 && text.charAt(text.length - 1) === "\n")
-            text = text + "\ufeff";
-        
-        // If there's no text, render a non-breaking space, or a placeholder if provided.
-        if(text.length === 0)
-            text = placeholder ?? "\ufeff";
+        segments = undefined;
 
-        // Is there a query we're supposed to highlight? If so, highlight it.
-        if($context && $context.highlightedWord) {
-            const query = $context.highlightedWord;
-            const lowerText = text.toLowerCase();
-            // Does this text contain the query? Highlight it.
-            if(lowerText.indexOf(query) >= 0) {
+        if(!editable || ($caret?.range?.start.node !== node)) {
 
-                // Find all the matches
-                const indices = [];
-                for(let i = 0; i < text.length; ++i) {
-                    if (lowerText.substring(i, i + query.length) === query) {
-                        indices.push(i);
+            // Replace any spaces at the beginning or end of the string with explicit non-breaking spaces to ensure that they render.
+            text = replaceMultipleSpacesWithNonBreakingSpaces(text);
+
+            // If the text ends with a newline, render a non-breaking space at the end.
+            if(text.length > 0 && text.charAt(text.length - 1) === "\n")
+                text = text + "\ufeff";
+            
+            // If there's no text, render a non-breaking space, or a placeholder if provided.
+            if(text.length === 0)
+                text = placeholder ?? "\ufeff";
+
+            // Is there a query we're supposed to highlight? If so, highlight it.
+            if($context && $context.highlightedWord) {
+                const query = $context.highlightedWord;
+                const lowerText = text.toLowerCase();
+                // Does this text contain the query? Highlight it.
+                if(lowerText.indexOf(query) >= 0) {
+
+                    // Find all the matches
+                    const indices = [];
+                    for(let i = 0; i < text.length; ++i) {
+                        if (lowerText.substring(i, i + query.length) === query) {
+                            indices.push(i);
+                        }
                     }
+
+                    // Go through each one and construct contents for the span to return.
+                    segments = [];
+                    for(let i = 0; i < indices.length; i++) {
+                        // Push the text from the end of the last match or the start of the string.
+                        segments.push([text.substring(i === 0 ? 0 : indices[i - 1] + query.length, indices[i]), false]);
+                        segments.push([text.substring(indices[i], indices[i] + query.length), true]);
+                    }
+                    if(indices[indices.length - 1] < text.length - 1)
+                        segments.push([text.substring(indices[indices.length - 1] + query.length, text.length), false]);
                 }
 
-                // Go through each one and construct contents for the span to return.
-                segments = [];
-                for(let i = 0; i < indices.length; i++) {
-                    // Push the text from the end of the last match or the start of the string.
-                    segments.push([text.substring(i === 0 ? 0 : indices[i - 1] + query.length, indices[i]), false]);
-                    segments.push([text.substring(indices[i], indices[i] + query.length), true]);
-                }
-                if(indices[indices.length - 1] < text.length - 1)
-                    segments.push([text.substring(indices[indices.length - 1] + query.length, text.length), false]);
             }
-
         }
     }
 </script>
