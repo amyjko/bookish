e3368b45b022e72df2a657668b8805373e750f76
Amy J. Ko <ajko@uw.edu>
2023-02-05 21:27:06 -0800

    Export Unknown to package.

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,6 +5,7 @@ export { default as References } from './components/page/References.svelte';
 export { default as PossibleChapter } from './components/page/PossibleChapter.svelte';
 export { default as Media } from './components/page/Media.svelte';
 export { default as Print } from './components/page/Print.svelte';
+export { default as Unknown } from './components/page/Unknown.svelte';
 export { default as Glossary } from './components/page/Glossary.svelte';
 export { default as Edition } from './components/page/Edition.svelte';
 export { default as Feedback } from './components/app/Feedback.svelte';
