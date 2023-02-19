54d653227513c777a251b81984d64bbf6421e1d0
Amy J. Ko <ajko@uw.edu>
2023-02-18 17:01:06 -0800

    Fixed caret position on footnotes.

--- src/lib/components/chapter/Footnote.svelte
+++ src/lib/components/chapter/Footnote.svelte
@@ -43,11 +43,11 @@
             id={'footnote-' + number}
             label="footnote, press escape to edit"
         >
-            <sup slot="interactor" class="bookish-footnote-symbol">{letter}</sup
-            >
             <!-- We prevent default on the span to prevent mouse events from bubbling up to the footnote symbol. This is key for two reasons:
                  1) clicks on the footnote select the footnote atom node itself
                  2) we want to be able to click on footnote text and we can't do that if the footnote sets the caret to the atom after clicks. -->
+            <sup slot="interactor" class="bookish-footnote-symbol">{letter}</sup
+            >
             <span
                 slot="content"
                 class={`bookish-footnote ${
@@ -80,7 +80,7 @@
         color: var(--bookish-paragraph-color);
         font-weight: 300;
         font-size: var(--bookish-small-font-size);
-        line-height: 1.5em;
+        /* line-height: 1.5em; */
     }
 
     .bookish-footnote-symbol {
@@ -90,26 +90,14 @@
         position: relative;
     }
 
-    /* This enables us to layout the numbers and letters in a neat right-aligned floating grid. */
-    .bookish-footnote {
-        position: relative;
-    }
-
     .bookish-footnote {
         display: block;
         line-height: var(--bookish-paragraph-line-height-tight);
-        margin-left: 1rem;
-        margin-bottom: 1rem;
-        text-align: left;
     }
 
     .bookish-footnote .bookish-footnote-symbol {
         display: block;
         float: left;
-        margin-top: -0.5em;
-        position: absolute;
-        left: -1.75em;
-        text-align: right;
-        width: 1.5em; /* Just enough space for 2 digits, a bit of a hack */
+        margin-left: -1em;
     }
 </style>
