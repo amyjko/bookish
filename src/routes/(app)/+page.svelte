e0cc7f471b30395a43fb167b5bdaf4588d9dc8a8
Amy J. Ko <ajko@uw.edu>
2023-06-24 14:47:47 -0700

    Improved FAQ.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -9,8 +9,20 @@
 
 <Section>
     <Lead
-        ><Large>Bookish</Large> (beta*) is a platform for writing simple, beautiful,
-        accessible online books.</Lead
+        ><Large
+            >Bookish <sup style="font-size:25%; font-weight: 400;">beta</sup
+            ></Large
+        > is a platform for writing simple, beautiful, accessible online books, like
+        <Link external to="https://criticallyconsciouscomputing.org/">this</Link
+        >, <Link
+            external
+            to="https://faculty.washington.edu/ajko/books/foundations-of-information"
+            >this</Link
+        >, or <Link
+            external
+            to="https://faculty.washington.edu/ajko/books/cooperative-software-development"
+            >this</Link
+        >.</Lead
     >
 
     <Head>Simple</Head>
@@ -40,84 +52,4 @@
             >learn more</Link
         >.
     </Paragraph>
-
-    <Head>FAQs</Head>
-
-    <Paragraph>
-        <strong>What does beta mean?</strong> Beta means that we are still
-        working toward a <Link
-            to="https://github.com/amyjko/bookish/milestones/1.0">1.0</Link
-        >, so there are still many known issues that may disrupt authoring, or
-        even lead to data loss. The reading experience, however, is quite
-        stable. If you'd like to accelerate progress toward a 1.0, <Link
-            to="mailto:ajko@uw.edu">write Amy</Link
-        >. We do not have a timeline for the 1.0 release, but the project is
-        actively progressing.
-    </Paragraph>
-
-    <Paragraph>
-        <strong>Is Bookish a company?</strong> No. At the moment, it is strictly
-        an open source project maintained by <Link to="https://amyjko.phd"
-            >Amy J. Ko</Link
-        >, and the occasional contributor from around the world. It has no
-        revenue stream or organization to support it, although Amy's role as
-        Professor at the University of Washington Information School, and the
-        public funding that supports the university, is a key part of what
-        enables Amy to work on the project. In particular, some National Science
-        Foundation grants (<Link
-            to="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1703734"
-            >#1703734</Link
-        >, <Link to="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2031265"
-            >#2031265</Link
-        >), have partially funded her time to work on book writing
-        infrastructure. Not having an organization has tradeoffs. For example,
-        we can't take money, and therefore can use money to grow a team to
-        better support the project. However, because we don't rely on a revenue
-        stream, it means we can focus on the values above without them being
-        warped by capital.
-    </Paragraph>
-
-    <Paragraph
-        ><strong>Can Bookish export PDFs?</strong> No, although you can visit a chapter
-        or show all chapters on a page and then have your browser generate a PDF.
-        We recognize that PDFs are still widely used, but since they are extremely
-        difficult to make accesssible to people who rely on screen readers, they
-        do not align with the project's values. Bookish is web-first and web-only
-        for this reason.</Paragraph
-    >
-
-    <Paragraph
-        ><strong>Can you support my custom domain?</strong> Yes, but with
-        effort. By default, books published on <em>bookish.press</em> are hosted
-        on this domain. We intend our 1.0 to have support for exporting an HTML,
-        CSS, JavaScript bundle which you will be able to upload and host
-        yourself on other domains. This will have the downside of requiring you
-        to rexport and reupload any time you make changes to your book and to
-        manage your own hosting. Another option is
-        <Link to="https://en.wikipedia.org/wiki/Domain_masking"
-            >domain masking</Link
-        >: some domain registrars will allow you to forward all requests to a
-        domain to your Bookish book, making it look like your book is hosted on
-        your domain. You'll have to check your domain provider's documentation
-        on how to do this. Finally, you can always choose a Bookish URL (e.g.,
-        bookish.press/mybook) if you're okay having the Bookish name as part of
-        your book's URL.</Paragraph
-    >
-
-    <Paragraph
-        ><strong>Does Bookish support multiple editions?</strong> Yes, each Bookish
-        book is a series of editions. You can make a new edition at any time, which
-        copies your latest edition, and then you can make edits and describe a summary
-        of what changed.</Paragraph
-    >
-
-    <Paragraph
-        ><strong>Is Bookish WCAG compliant?</strong>We aspire to meet and exceed
-        all <Link to="https://www.w3.org/TR/WCAG21/">WCAG 2.1 guidelines</Link>.
-        We are always finding new ways to verify and improve accessibility,
-        however, and web standards are always evolving, so accessibility will
-        always be an ongoing project. See our <Link
-            to="https://github.com/amyjko/bookish/issues">open issues</Link
-        > for any open accessibility problems.</Paragraph
-    >
 </Section>
--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -23,50 +23,25 @@
         a side project and continues to use it extensively in her classes.
     </Paragraph>
 
-    <Head id="beta">Beta</Head>
-
-    <Paragraph
-        >Bookish is currently in <em>beta</em>. That means there are likely to
-        be defects, including those that result in loss of content. We'll remove
-        this label when we're confident that the platform can reliably serve
-        both readers and writers. See our <Link
-            to="https://github.com/amyjko/bookish/issues?q=is%3Aissue+is%3Aopen+label%3Adefect"
-            >list of defects</Link
-        > to check our progress (and help, if you are able).</Paragraph
-    >
-
-    <Head id="costs">Costs</Head>
-
-    <Paragraph>
-        Using Bookish to write books will always be free. However, hosting is
-        not free and particularly widely read books can incur costs that Amy
-        can't pay alone. She is considering models for covering bandwidth costs
-        from reading such per-book fees or donations. If you have ideas, <Link
-            to="https://github.com/amyjko/bookish/issues/251"
-            >join the discussion</Link
-        >.
-    </Paragraph>
-
-    <Head id="vision">Vision</Head>
-
+    <Head id="scope">What do you publish?</Head>
     <Paragraph>
         As with any publisher, Bookish has an editorial policy and philosophy.
         Much like university publishers (e.g., <Link
             to="https://mitpress.mit.edu/about/">MIT Press</Link
         >), Amy views the mission of Bookish Press as <em>scholarly</em>: books
         on the platform should advance knowledge, educate the public, and be
-        informed by other scholarly work. However, Bookish is particularly
-        interested in works that advance progress toward a society that
+        informed by other scholarly work, through fiction and non-fiction.
+        However, Bookish is particularly interested in works that advance
+        progress toward a society that
         <em
             >radically embraces the beautiful diversity of humanity and nature
             and seeks to dismantle systems that get in the way.</em
-        > While this includes works broadly explicitly about social justice topics
+        >
+        While this includes works broadly explicitly about social justice topics
         such as diversity, equality, equity, inclusion, justice, accessibility, sustainability,
         indigienous sovereignty, and oppression, it also includes works that discuss
         more strictly technical ideas in sociotechnical ways.
     </Paragraph>
-
-    <Head id="editing">Editing</Head>
 
     <Paragraph
         >For now, anyone can create an account and write books, but publishing
@@ -85,7 +60,103 @@
         different editorial vision.
     </Paragraph>
 
-    <Head id="feedback">Feedback</Head>
+    <Head id="beta">What does "beta" mean?</Head>
+
+    <Paragraph>
+        Beta means that we are still working toward a <Link
+            to="https://github.com/amyjko/bookish/milestones/1.0">1.0</Link
+        >, so there are still many known issues that may disrupt authoring, or
+        even lead to data loss. The reading experience, however, is quite
+        stable. If you'd like to accelerate progress toward a 1.0, <Link
+            to="mailto:ajko@uw.edu">write Amy</Link
+        >. We do not have a timeline for the 1.0 release, but the project is
+        actively progressing.
+    </Paragraph>
+
+    <Head id="costs">What does it cost?</Head>
+
+    <Paragraph>
+        Using Bookish to write books will always be free. However, hosting is
+        not free and particularly widely read books can incur costs that Amy
+        can't pay alone. She is considering models for covering bandwidth costs
+        from reading such per-book fees or donations. If you have ideas, <Link
+            to="https://github.com/amyjko/bookish/issues/251"
+            >join the discussion</Link
+        >.
+    </Paragraph>
+
+    <Head id="business">Is Bookish a business?</Head>
+    <Paragraph>
+        No. At the moment, it is strictly an open source project maintained by <Link
+            to="https://amyjko.phd">Amy</Link
+        >, and the occasional contributor from around the world. It has no
+        revenue stream or organization to support it, although Amy's role as
+        Professor at the University of Washington Information School, and the
+        public funding that supports the university, is a key part of what
+        enables Amy to work on the project. In particular, some National Science
+        Foundation grants (<Link
+            to="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1703734"
+            >#1703734</Link
+        >, <Link to="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2031265"
+            >#2031265</Link
+        >), have partially funded her time to work on book writing
+        infrastructure. Not having an organization has tradeoffs. For example,
+        we can't take money, and therefore can use money to grow a team to
+        better support the project. However, because we don't rely on a revenue
+        stream, it means we can focus on the values above without them being
+        warped by capital.
+    </Paragraph>
+
+    <Head id="pdf">PDFs?</Head>
+    <Paragraph
+        >No, although you can visit a chapter or show all chapters on a page and
+        then have your browser generate a PDF. We recognize that PDFs are still
+        widely used, but since they are extremely difficult to make accesssible
+        to people who rely on screen readers, they do not align with the
+        project's values. Bookish is web-first and web-only for this reason.</Paragraph
+    >
+
+    <Head id="pdf">Custom domains?</Head>
+    <Paragraph
+        >Yes, but with effort. By default, books published on <em
+            >bookish.press</em
+        >
+        are hosted on this domain. We intend our 1.0 to have support for exporting
+        an HTML, CSS, JavaScript bundle which you will be able to upload and host
+        yourself on other domains. This will have the downside of requiring you to
+        rexport and reupload any time you make changes to your book and to manage
+        your own hosting. Another option is
+        <Link to="https://en.wikipedia.org/wiki/Domain_masking"
+            >domain masking</Link
+        >: some domain registrars will allow you to forward all requests to a
+        domain to your Bookish book, making it look like your book is hosted on
+        your domain. You'll have to check your domain provider's documentation
+        on how to do this. Finally, you can always choose a Bookish URL (e.g.,
+        bookish.press/mybook) if you're okay having the Bookish name as part of
+        your book's URL.</Paragraph
+    >
+
+    <Head id="pdf">Multiple editions?</Head>
+    <Paragraph
+        >Yes, each Bookish book is a series of editions. You can make a new
+        edition at any time, which copies your latest edition, and then you can
+        make edits and describe a summary of what changed.</Paragraph
+    >
+
+    <Head id="pdf">WCAG compliance?</Head>
+
+    <Paragraph
+        >We aspire to meet and exceed all <Link
+            to="https://www.w3.org/TR/WCAG21/">WCAG 2.1 guidelines</Link
+        >. We are always finding new ways to verify and improve accessibility,
+        however, and web standards are always evolving, so accessibility will
+        always be an ongoing project. See our <Link
+            to="https://github.com/amyjko/bookish/issues?q=is%3Aissue+is%3Aopen+label%3Adefect"
+            >open issues</Link
+        > for any open accessibility problems.</Paragraph
+    >
+
+    <Head id="feedback">How do I give feedback?</Head>
 
     <Paragraph>
         Do you have feedback or want to contribute to the plaform? The project
